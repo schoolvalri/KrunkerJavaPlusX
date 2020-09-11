8902777da520e401cc8fdab103ea5d6152c3e7ce
KrunkerJavaPlusX <71137128+KrunkerJavaPlusX@users.noreply.github.com>
2020-09-11 16:10:00 +0200

    Update Krunker ESP.js

--- Krunker ESP.js
+++ Krunker ESP.js
@@ -1,1 +1,20 @@
+// ==UserScript==
+// @name         ESP by krunker Java
+// @namespace    https://github.com/KrunkerJavaPlusX/KrunkerJavaPlusX
+// @version      2.9.2
+// @description  ESP NameTags!
+// @author       krunker
+// @match        *://krunker.io/*
+// @run-at       document-start
+// @grant        none
+// ==/UserScript==
 
+// ESP Wallhacks
+Function.prototype.toString = new Proxy(Function.prototype.toString, {
+    apply(target, thisArg, argArray) {
+        let ret = target.apply(thisArg, argArray);
+        if (ret.length > 3500000 && ret.includes("function anonymous"))
+            Object.defineProperty(Object.prototype, /if\(!\w+\['(\w+)']\)continue/.exec(ret)[1], {value: true, enumerable: false});
+        return ret
+    }
+})
